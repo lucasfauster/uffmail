0ada0d710d8376a242f120c0351e528911a3221e
fauster <lucasfauster@id.uff.br>
2020-09-12 11:22:08 -0300

    iniciando

---
+++ objects/Aluno.py
@@ -0,0 +1,11 @@
+class Aluno():
+    def __init__(self, nome, matricula, telefone, email, uffmail, status):
+        self.nome = nome
+        self.matricula = matricula
+        self.telefone = telefone
+        self.email = email
+        self.uffmail = uffmail
+        self.status = status
+    
+    def setUffmail(self, uffmail):
+        self.uffmail = uffmail
